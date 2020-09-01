666dc2e7493d2b09cc54a3f7a611144cbe45ab18
47467 <47467@192.168.80.150>
2020-09-01 12:38:58 +0200

    Oppgave 2

---
+++ Innlevering/src/no/hvl/Dat100/Karakter.java
@@ -0,0 +1,35 @@
+package no.hvl.Dat100;
+
+public class Karakter {
+	
+public static void main(String[] args) {
+	
+	for (int k = 1; k <= 10; k++) {
+	String sum = javax.swing.JOptionPane.showInputDialog("Poengsum:");
+	int poeng = Integer.parseInt(sum);
+	
+	if (poeng <= 39) {
+		System.out.println("F");
+	}
+	if (poeng >= 40 && poeng <= 49) {
+		System.out.println("E");
+	}
+	if (poeng >= 50 && poeng <= 59) {
+		System.out.println("D");
+		}
+	
+	if (poeng >= 60 && poeng <= 79) {
+		System.out.println("C");
+		}
+	
+	if (poeng >= 80 && poeng <= 89) {
+		System.out.println("B");
+		}
+	
+	if (poeng >= 90 && poeng <= 100) {
+		System.out.println("A");
+		}
+	}
+	}
+
+}
